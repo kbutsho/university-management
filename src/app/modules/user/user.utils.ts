d7abc58411e0362f0543dd364af8d359f4c6a7c4
Kaushik Biswas <kbutsho@gmail.com>
2023-06-16 17:21:25 +0600

    added

---
+++ src/app/modules/user/user.utils.ts
@@ -0,0 +1,55 @@
+import { IAcademicSemester } from "../academicSemester/academicSemester.interface";
+import { User } from "./user.model";
+
+export const findLastStudentId = async (): Promise<string | undefined> => {
+  const lastStudent = await User.findOne(
+    {
+      role: "student",
+    },
+    { id: 1, _id: 0 }
+  )
+    .sort({
+      createdAt: -1,
+    })
+    .lean();
+
+  return lastStudent?.id ? lastStudent.id.substring(4) : undefined;
+};
+
+export const generateStudentId = async (
+  academicSemester: IAcademicSemester | null
+): Promise<string> => {
+  if (academicSemester === null) {
+    // Handle the null case here, such as returning a default value
+    return "00000"; // or any other default value you prefer
+  }
+  const currentId =
+    (await findLastStudentId()) || (0).toString().padStart(5, "0"); //00000
+  //increment by 1
+  let incrementedId = (parseInt(currentId) + 1).toString().padStart(5, "0");
+  //20 25
+  incrementedId = `${academicSemester.year.substring(2)}${
+    academicSemester.code
+  }${incrementedId}`;
+
+  return incrementedId;
+};
+
+export const findLastFacultyId = async (): Promise<string | undefined> => {
+  const lastFaculty = await User.findOne({ role: "faculty" }, { id: 1, _id: 0 })
+    .sort({
+      createdAt: -1,
+    })
+    .lean();
+
+  return lastFaculty?.id ? lastFaculty.id.substring(2) : undefined;
+};
+
+export const generateFacultyId = async (): Promise<string> => {
+  const currentId =
+    (await findLastFacultyId()) || (0).toString().padStart(5, "0");
+  let incrementedId = (parseInt(currentId) + 1).toString().padStart(5, "0");
+  incrementedId = `F-${incrementedId}`;
+
+  return incrementedId;
+};
---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import { bold, cyan, yellow } from 'colorette';
+import { Server } from 'http';
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config/index';
+import { errorlogger, logger } from './shared/logger';
+
+process.on('uncaughtException', error => {
+  errorlogger.error(error);
+  process.exit(1);
+});
+
+let server: Server;
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    logger.info(yellow(bold('Database connected successfully!')));
+    server = app.listen(config.port, () => {
+      logger.info(cyan(bold(`Server is running on port ${config.port}!`)));
+    });
+  } catch (err) {
+    errorlogger.error('Failed to connect database', err);
+  }
+
+  process.on('unhandledRejection', error => {
+    if (server) {
+      server.close(() => {
+        errorlogger.error(error);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
+}
+
+main();
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM is received');
+  if (server) {
+    server.close();
+  }
+});
